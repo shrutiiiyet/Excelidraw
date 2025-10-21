8d2c4573cde3d346281b3e83ceb2746bee63c25f
shrutiii <shrutijadhav4102@gmail.com>
2025-10-21 16:41:29 +0530

    Regenrated lock file

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,14 +1,5 @@
 import { PrismaClient } from "../generated/prisma/index";
 
-// declare global {
-//   var client: PrismaClient | undefined;
-// }
-
-// const client = global.client ?? new PrismaClient();
-
-// if (process.env.NODE_ENV !== "production") {
-//   globalThis.client = client;
-// }
 const client = new PrismaClient();
 
 export { client };
